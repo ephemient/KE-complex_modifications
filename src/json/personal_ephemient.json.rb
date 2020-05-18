93ac8893017d7aacab53fa560324bfad2718796f
Daniel Lin <ephemient@gmail.com>
2020-05-17 23:08:30 -0400

    Update personal_ephemient

--- src/json/personal_ephemient.json.rb
+++ src/json/personal_ephemient.json.rb
@@ -4,54 +4,56 @@
 require 'json'
 require_relative '../lib/karabiner.rb'
 
+def remote_frontmost_application_if
+  Karabiner.frontmost_application_if(
+    %w[virtual_machine vnc remote_desktop],
+    bundle_identifiers: %w[^com\.blade\.shadow-macos$],
+  )
+end
+def remote_frontmost_application_unless
+  Karabiner.frontmost_application_unless(
+    %w[virtual_machine vnc remote_desktop],
+    bundle_identifiers: %w[^com\.blade\.shadow-macos$],
+  )
+end
+
 APPLE_VENDOR_ID = 1452
 def apple_device_if
-  {
-    type: 'device_if',
-    identifiers: [{description: 'Apple', vendor_id: APPLE_VENDOR_ID}],
-  }
+  {type: :device_if, identifiers: [{description: :Apple, vendor_id: APPLE_VENDOR_ID}]}
 end
 def apple_device_unless
-  {
-    type: 'device_unless',
-    identifiers: [{description: 'Apple', vendor_id: APPLE_VENDOR_ID}],
-  }
+  {type: :device_unless, identifiers: [{description: :Apple, vendor_id: APPLE_VENDOR_ID}]}
 end
 
 MICROSOFT_VENDOR_ID = 1118
-MICROSOFT_KEYBOARD_PRODUCTS = {1936 => 'Wedge Mobile Keyboard'}
-MICROSOFT_MOUSE_PRODUCTS = {1954 => 'Sculpt Comfort Mouse'}
+MICROSOFT_KEYBOARD_PRODUCTS = {1936 => 'Wedge Mobile Keyboard'}.freeze
+MICROSOFT_MOUSE_PRODUCTS = {1954 => 'Sculpt Comfort Mouse'}.freeze
 def microsoft_keyboard_device_if
   {
-    type: 'device_if',
-    identifiers: MICROSOFT_KEYBOARD_PRODUCTS.map { |id, name|
-      {description: "Microsoft #{name}", vendor_id: MICROSOFT_VENDOR_ID, product_id: id}
-    },
+    type: :device_if,
+    identifiers: MICROSOFT_KEYBOARD_PRODUCTS.map do |id, desc|
+      {description: "Microsoft #{desc}", vendor_id: MICROSOFT_VENDOR_ID, product_id: id}
+    end,
   }
 end
 def microsoft_mouse_device_if
   {
-    type: 'device_if',
-    identifiers: MICROSOFT_MOUSE_PRODUCTS.map { |id, name|
-      {description: name, vendor_id: MICROSOFT_VENDOR_ID, product_id: id}
-    },
+    type: :device_if,
+    identifiers: MICROSOFT_MOUSE_PRODUCTS.map do |id, desc|
+      {description: "Microsoft #{desc}", vendor_id: MICROSOFT_VENDOR_ID, product_id: id}
+    end,
   }
 end
 
 def command_option_swap_manipulators
-  buttons = %w[command option]
-  %w[left right].flat_map { |side|
-    [buttons, buttons.reverse].map { |from, to|
-      {
-        type: 'basic',
-        from: {
-          key_code: "#{side}_#{from}",
-          modifiers: {optional: %w[any]}
-        },
-        to: [{key_code: "#{side}_#{to}"}],
-      }
+  buttons = %i[left_command left_option right_command right_option]
+  buttons.map.with_index do |button, i|
+    {
+      type: :basic,
+      from: {key_code: button, modifiers: {optional: %i[any]}},
+      to: [{key_code: buttons[i ^ 1]}],
     }
-  }
+  end
 end
 
 def main
@@ -59,118 +61,128 @@ def main
     title: 'Personal rules (@ephemient)',
     rules: [
       {
+        description: 'Double Shift to Caps Lock (or mouse keys)',
+        manipulators: [
+          {
+            type: :basic,
+            from: {
+              simultaneous: [{key_code: :left_shift}, {key_code: :right_shift}],
+              simultaneous_options: {detect_key_down_uninterruptedly: true, key_up_when: :all},
+              modifiers: {optional: %i[any]},
+            },
+            to_if_alone: [{key_code: :caps_lock, hold_down_milliseconds: 200}],
+            to: {set_variable: {name: :alternate_mode, value: 1}},
+            to_after_key_up: {set_variable: {name: :alternate_mode, value: 0}},
+          },
+          *{
+            button1: %i[spacebar return_or_enter],
+            button2: %i[left_control right_control caps_lock],
+            button3: %i[left_command left_option right_option right_command],
+          }.flat_map do |button, keys|
+            keys.map do |key|
+              {
+                type: :basic,
+                conditions: [{type: :variable_if, name: :alternate_mode, value: 1}],
+                from: {key_code: key, modifiers: {optional: %i[any]}},
+                to: [{pointing_button: button}],
+              }
+            end
+          end,
+          *{
+            w: {y: -1536}, d: {x: 1536}, a: {x: -1536}, s: {y: 1536},
+            h: {horizontal_wheel: -48}, l: {horizontal_wheel: 48},
+            j: {vertical_wheel: -48}, k: {vertical_wheel: 48},
+            f: {speed_multiplier: 2.0}, g: {speed_multiplier: 0.5},
+          }.map do |key, mouse|
+            {
+              type: :basic,
+              conditions: [{type: :variable_if, name: :alternate_mode, value: 1}],
+              from: {key_code: key, modifiers: {mandatory: %i[any]}},
+              to: {mouse_key: mouse},
+            }
+          end,
+          {
+            type: :basic,
+            conditions: [{type: :variable_if, name: :alternate_mode, value: 1}],
+            from: {any: :key_code, modifiers: {optional: %i[any]}},
+            to: [],
+          },
+        ],
+      },
+      {
         description: 'Application to Option on Microsoft keyboards',
         manipulators: [
           {
-            type: 'basic',
-            conditions: [
-              microsoft_keyboard_device_if,
-              Karabiner.frontmost_application_unless(
-                %w[virtual_machine vnc remote_desktop],
-                bundle_identifiers: %w[^com\.blade\.shadow-macos$],
-              )
-            ],
-            from: {key_code: 'application'},
-            to: [{key_code: 'right_option'}],
-          }
-        ]
+            type: :basic,
+            conditions: [microsoft_keyboard_device_if, remote_frontmost_application_unless],
+            from: {key_code: :application},
+            to: [{key_code: :right_option}],
+          },
+        ],
       },
       {
         description: 'Microsoft mouse button to Mission Control',
         manipulators: [
           {
-            type: 'basic',
-            parameters: {'basic.to_if_held_down_threshold_milliseconds' => 250},
-            conditions: [
-              microsoft_mouse_device_if,
-              Karabiner.frontmost_application_unless(
-                %w[virtual_machine vnc remote_desktop],
-                bundle_identifiers: %w[^com\.blade\.shadow-macos$],
-              )
-            ],
-            from: {key_code: 'left_command'},
-            to_if_alone: [{key_code: 'mission_control'}],
-            to_if_held_down: [{key_code: 'mission_control'}],
+            type: :basic,
+            conditions: [microsoft_mouse_device_if, remote_frontmost_application_unless],
+            from: {key_code: :left_command, modifiers: {mandatory: %i[control]}},
+            to_if_alone: [{key_code: :dashboard}],
           },
           {
-            type: 'basic',
-            conditions: [
-              microsoft_mouse_device_if,
-              Karabiner.frontmost_application_unless(
-                %w[virtual_machine vnc remote_desktop],
-                bundle_identifiers: %w[^com\.blade\.shadow-macos$],
-              )
-            ],
-            from: {
-              key_code: 'tab',
-              modifiers: {optional: %w[left_control]}
-            },
-            to: [{key_code: 'left_arrow'}],
+            type: :basic,
+            parameters: {:'basic.to_if_held_down_threshold_milliseconds' => 250},
+            conditions: [microsoft_mouse_device_if, remote_frontmost_application_unless],
+            from: {key_code: :left_command, modifiers: {optional: %i[any]}},
+            to_if_alone: [{key_code: :mission_control}],
+            to_if_held_down: [{key_code: :mission_control}],
           },
           {
-            type: 'basic',
-            conditions: [
-              microsoft_mouse_device_if,
-              Karabiner.frontmost_application_unless(
-                %w[virtual_machine vnc remote_desktop],
-                bundle_identifiers: %w[^com\.blade\.shadow-macos$],
-              )
-            ],
-            from: {
-              key_code: 'delete_or_backspace',
-              modifiers: {optional: %w[left_control]}
-            },
-            to: [{key_code: 'right_arrow'}],
+            type: :basic,
+            conditions: [microsoft_mouse_device_if, remote_frontmost_application_unless],
+            from: {key_code: :tab, modifiers: {mandatory: %i[left_control any]}},
+            to: [{key_code: :left_arrow, modifiers: %i[control]}],
           },
-        ]
+          {
+            type: :basic,
+            conditions: [microsoft_mouse_device_if, remote_frontmost_application_unless],
+            from: {key_code: :delete_or_backspace, modifiers: {mandatory: %i[left_control any]}},
+            to: [{key_code: :right_arrow, modifiers: %i[control]}],
+          },
+        ],
       },
       {
         description: 'Caps Lock to Left Control (or Escape)',
         manipulators: [
           {
-            type: 'basic',
+            type: :basic,
             conditions: [Karabiner.frontmost_application_if(%w[terminal vi])],
-            from: {key_code: 'caps_lock'},
-            to: [{key_code: 'left_control', lazy: true}],
-            to_if_alone: [{key_code: 'escape'}],
+            from: {key_code: :caps_lock},
+            to: [{key_code: :left_control, lazy: true}],
+            to_if_alone: [{key_code: :escape}],
           },
           {
-            type: 'basic',
-            from: {
-              key_code: 'caps_lock',
-              modifiers: {optional: %w[any]}
-            },
-            to: [{key_code: 'left_control'}],
-          }
-        ]
+            type: :basic,
+            from: {key_code: :caps_lock, modifiers: {optional: %i[any]}},
+            to: [{key_code: :left_control}],
+          },
+        ],
       },
       {
         description: 'Alt/GUI to Command/Option on non-Apple keyboards',
-        manipulators: command_option_swap_manipulators.map { |manipulator|
+        manipulators: command_option_swap_manipulators.map do |manipulator|
           manipulator.merge({
-            conditions: [
-              apple_device_unless,
-              Karabiner.frontmost_application_unless(
-                %w[virtual_machine vnc remote_desktop],
-                bundle_identifiers: %w[^com\.blade\.shadow-macos$],
-              )
-            ],
+            conditions: [apple_device_unless, remote_frontmost_application_unless],
           })
-        },
+        end,
       },
       {
-        description: 'Command/Option to Alt/GUI to Command/Option in RDP',
-        manipulators: command_option_swap_manipulators.map { |manipulator|
+        description: 'Command/Option to Alt/GUI to Command/Option when remote',
+        manipulators: command_option_swap_manipulators.map do |manipulator|
           manipulator.merge({
-            conditions: [
-              apple_device_if,
-              Karabiner.frontmost_application_if(
-                %w[virtual_machine vnc remote_desktop],
-                bundle_identifiers: %w[^com\.blade\.shadow-macos$],
-              )
-            ],
+            conditions: [apple_device_if, remote_frontmost_application_if],
           })
-        },
+        end,
       },
     ],
   )
